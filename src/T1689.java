f6089de67d6fe98e363cfd6164e48dc5bb67f413
xiaozhu-sorce <790362227@qq.com>
2021-05-26 17:49:52 +0800

    贪心算法

---
+++ src/T1689.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class T1689 {
+    public int minPartitions(String n) {
+        int[] a = new int[10];
+        int s = 0,m;
+        Arrays.fill(a, 0); //填充数组
+        for (int  i=0;i < n.length();i++){
+            m = n.charAt(i) - '0'; //依次取出字符串中的每个字符
+            a[m]++;
+            if(m>s) s=m;
+        }
+        return s;
+    }
+}
