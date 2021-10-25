9c3d1dc1272acffe701ca7a7cb1e0fc2640f8d3d
xiaozhu-sorce <790362227@qq.com>
2021-10-25 20:56:08 +0800

    add:Matrix(Z搜索)

---
+++ src/Matrix/T0240_SearchA2DMatrixII.java
@@ -0,0 +1,19 @@
+package Matrix;
+
+public class T0240_SearchA2DMatrixII {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+        int x = 0, y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            }
+            if (matrix[x][y] > target) {
+                --y;
+            } else {
+                ++x;
+            }
+        }
+        return false;
+    }
+}
