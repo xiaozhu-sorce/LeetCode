347f4847d7cd5b4f37f4f8caf731414c5bf65ad9
xiaozhu-sorce <790362227@qq.com>
2021-11-11 19:39:00 +0800

    24. Swap Nodes in Pairs

--- src/LinkedList/T0006_ReverseLinkedList.java
+++ src/LinkedList/T0006_ReverseLinkedList.java
@@ -1,6 +1,13 @@
 package LinkedList;
 
 public class T0006_ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public ListNode reverseList(ListNode head) {
         ListNode p = head;
         ListNode s;
---
+++ src/LinkedList/T0024_SwapNodesinPairs.java
@@ -0,0 +1,34 @@
+package LinkedList;
+
+public class T0024_SwapNodesinPairs {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode swapPairs(ListNode head) {
+        ListNode p = head;
+        ListNode temp,pre = null;
+        if (head!=null && head.next!=null)
+            head = head.next;
+        while(p!=null && p.next != null && p.next.next !=null){
+            temp  = p.next;
+            p.next = temp.next;
+            temp.next = p;
+            if(pre != null)
+                pre.next = temp;
+            pre = p;
+            p = p.next;
+        }
+        if(p!=null && p.next!=null){
+            temp  = p.next;
+            if(pre!=null)
+                pre.next = temp;
+            temp.next = p;
+            p.next = null;
+        }
+        return head;
+    }
+}
