53ad9306ed2d9b7162bbd13fce533d32bbbe5289
xiaozhu-sorce <790362227@qq.com>
2021-12-10 11:50:52 +0800

    236. Lowest Common Ancestor of a Binary Tree

--- src/Tree/T0222_CountCompleteTreeNodes.java
+++ src/Tree/T0222_CountCompleteTreeNodes.java
@@ -21,4 +21,29 @@ public class T0222_CountCompleteTreeNodes {
         int right = countNodes2(root.right);
         return count;
     }
+
+    //时间复杂度最低 O（logN*logN）
+    public int countNodes3(TreeNode root) {
+        TreeNode le = root;
+        TreeNode ri = root;
+        int hl = 0;
+        int hr = 0;
+        while(le != null){
+            le = le.left;
+            hl++;
+        }
+
+        while(ri != null){
+            ri = ri.right;
+            hr++;
+        }
+
+        if(hl == hr){
+            return (int)Math.pow(2, hl) - 1;
+        }
+
+        int count1 = countNodes3(root.left);
+        int count2 = countNodes3(root.right);
+        return count1 + count2 + 1;
+    }
 }
