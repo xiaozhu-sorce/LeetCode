3c921c8885b01db34f1b70a41b67ad273054aad0
xiaozhu-sorce <790362227@qq.com>
2022-01-20 21:55:35 +0800

    2029. Stone Game IX

---
+++ src/Greedy/T2029_StoneGameIX.java
@@ -0,0 +1,9 @@
+package Greedy;
+
+public class T2029_StoneGameIX {
+    public boolean stoneGameIX(int[] stones) {
+        int[] cnts = new int[3];
+        for (int i : stones) cnts[i % 3]++;
+        return cnts[0] % 2 == 0 ? !(cnts[1] == 0 || cnts[2] == 0) : !(Math.abs(cnts[1] - cnts[2]) <= 2);
+    }
+}
