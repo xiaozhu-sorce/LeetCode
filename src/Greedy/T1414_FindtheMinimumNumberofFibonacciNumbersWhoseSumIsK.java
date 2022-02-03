d26d2bfa4cb2da527511ae038b9acd7738623486
xiaozhu-sorce <790362227@qq.com>
2022-02-03 21:56:24 +0800

    1414. Find the Minimum Number of Fibonacci Numbers Whose Sum Is K

---
+++ src/Greedy/T1414_FindtheMinimumNumberofFibonacciNumbersWhoseSumIsK.java
@@ -0,0 +1,23 @@
+package Greedy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class T1414_FindtheMinimumNumberofFibonacciNumbersWhoseSumIsK {
+    public int findMinFibonacciNumbers(int k) {
+        int a = 1, b = 1;
+        while (b <= k) {
+            int c = a + b;
+            a = b; b = c;
+        }
+        int ans = 0;
+        while (k != 0) {
+            if (k >= b) {
+                k -= b; ans++;
+            }
+            int c = b - a;
+            b = a; a = c;
+        }
+        return ans;
+    }
+}
