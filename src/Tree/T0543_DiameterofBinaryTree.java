c28c2435436602c51694f2c14fb2d524987bbc50
xiaozhu-sorce <790362227@qq.com>
2022-03-28 09:48:13 +0800

    543. Diameter of Binary Tree

---
+++ src/Tree/T0543_DiameterofBinaryTree.java
@@ -0,0 +1,22 @@
+package Tree;
+
+public class T0543_DiameterofBinaryTree {
+    int maxDiameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return maxDiameter;
+    }
+
+    int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        int myDiameter = leftMax + rightMax;
+        maxDiameter = Math.max(maxDiameter, myDiameter);
+
+        return 1 + Math.max(leftMax, rightMax);
+    }
+}
